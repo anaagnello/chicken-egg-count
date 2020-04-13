9816db972d34f1a0cff3a2be0d1d1615f3eea0bf
anaagnello <ana.agnello@outlook.com>
2020-04-12 18:02:37 -0700

    6 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ChickenListComponent } from './chicken-list/chicken-list.component';
 import { ChickenComponent } from './chicken/chicken.component';
 import { ChickenServiceService } from './chicken-service.service';
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
+      { path: '', component: ChickenListComponent },
     ])
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
-    ProductListComponent,
+    ChickenListComponent,
     ChickenComponent
   ],
   bootstrap: [ AppComponent ],
   providers: [ChickenServiceService]
 })
 export class AppModule { }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
--- src/app/chicken-service.service.ts
+++ src/app/chicken-service.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class ChickenServiceService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
+  getChickens() {
+    return this.http.get('/assets/shipping.json');
+  }
 }
